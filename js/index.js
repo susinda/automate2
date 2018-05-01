6500151db46f4d9264b307b859bb60476004bd53
Susinda <susinda.perera@arptel.com>
2018-05-01 16:16:39 +1000

    Datatable display added

--- js/index.js
+++ js/index.js
@@ -57,6 +57,19 @@ $(document).ready(function () {
     	  $("#nsDestApi").show();
     });
     
+  $("#example").DataTable( {
+  columnDefs: [ {
+      orderable: false,
+      className: 'select-checkbox',
+      targets:   0
+  } ],
+  select: {
+      style:    'os',
+      selector: 'td:first-child'
+  },
+  order: [[ 1, 'asc' ]]
+} );
+    
 });
 
 function nextTab(elem) {
