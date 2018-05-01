89e447bb3e4fa66759c2edf59d759916f945add4
Susinda <susinda.perera@arptel.com>
2018-05-01 18:19:03 +1000

    Manual data entering implemented

--- js/index.js
+++ js/index.js
@@ -20,8 +20,20 @@ $(document).ready(function () {
     $(".next-step").click(function (e) {
 
         var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
+        var tabName = $active.attr("name");
+        var canGoNext = false;
+        if (tabName === "step01") {
+        	canGoNext = validateStep1();
+        } else if (tabName === "step02") {
+        	canGoNext = validateStep2();
+        } else {
+        	canGoNext = false;
+        }
+        
+        if (canGoNext) {
+        	$active.next().removeClass('disabled');
+        	nextTab($active);
+        }
 
     });
     $(".prev-step").click(function (e) {
@@ -58,23 +70,54 @@ $(document).ready(function () {
     });
     
   $("#example").DataTable( {
-  columnDefs: [ {
-      orderable: false,
-      className: 'select-checkbox',
-      targets:   0
-  } ],
-  select: {
-      style:    'os',
-      selector: 'td:first-child'
-  },
-  order: [[ 1, 'asc' ]]
-} );
-    
+	  columnDefs: [ {
+	      orderable: false,
+	      className: 'select-checkbox',
+	      targets:   0
+	  } ],
+	  select: {
+		  style: 'multi',
+	      selector: 'td:first-child'
+	  },
+	  order: [[ 1, 'asc' ]]
+	      
+   });
+
 });
+
 
 function nextTab(elem) {
     $(elem).next().find('a[data-toggle="tab"]').click();
 }
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
+function validateStep1() {
+	console.log("validateStep1 called");
+	var textVal = $("#txtManual").val();
+	if (textVal === "") {
+		return false;
+	} else {
+		var dataSet = [];
+		if (textVal.includes(",") ) {
+			var res = textVal.split(",");
+			var arrayLength = res.length;
+			for (var i = 0; i < arrayLength; i++) {
+			    dataSet.push(['', 'NA', 'NA', res[i]]);
+			}
+			
+		} else {
+			dataSet.push(['', 'NA', 'NA', textVal])
+		}
+		
+		$('#example').dataTable().fnClearTable();
+		$('#example').dataTable().fnAddData(dataSet);
+		return true;
+	}
+}
+
+function validateStep2() {
+	console.log("validateStep1 called");
+	return true;
 }
